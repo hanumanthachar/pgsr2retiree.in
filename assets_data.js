6d1a0ee7cc5789d641d84af40a196465dda87f29
hanumanthachar <4pb9mjmy6j@privaterelay.appleid.com>
2026-07-20 07:35:10 +0530

    Update assets_data.js

--- assets_data.js
+++ assets_data.js
@@ -1232,7 +1232,7 @@ const COMPENDIUM_DATA = {
   ],
   "aabhaarStart": 666,
   "faqs": [],
-  "otherInfo": [
+  "otherInfo": [Other USeful Information
   {
     "id": "other-01",
     "title": "Incometax-89(1)-Relief",
